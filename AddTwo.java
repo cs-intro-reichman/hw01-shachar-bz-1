e8e3c63d3a3fb18062fee19b5a9533fc651eaf79
shachar-bz <shachar.benzur@runi.ac.il>
2023-12-20 14:45:40 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -1,11 +1,11 @@
 public class AddTwo {
-public static void main(String[] args) {
+ public static void main(String[] args) {
 // choose two numbers and print their sum
 
- int num1 = Integer.parseInt(args[0]);
- int num2 = Integer.parseInt(args[1]);
+  int num1 = Integer.parseInt(args[0]);
+  int num2 = Integer.parseInt(args[1]);
 
- System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+  System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
  }
 }
 
--- Coins.java
+++ Coins.java
@@ -1,13 +1,13 @@
 public class Coins {
-public static void main(String[] args) {
+ public static void main(String[] args) {
 //  return the remainder of a number in quarters and a cents
 
- int num1 = Integer.parseInt(args[0]);
+  int num1 = Integer.parseInt(args[0]);
  
- int quarter = num1/25;
- int cent = num1%25;
+  int quarter = num1/25;
+  int cent = num1%25;
  
- System.out.println( "Use " + quarter + " quarters and " + cent + " cents ");
+  System.out.println( "Use " + quarter + " quarters and " + cent + " cents ");
  }
 }
 
